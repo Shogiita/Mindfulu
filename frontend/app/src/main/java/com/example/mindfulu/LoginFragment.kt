a4d06d2a7bcee1065238a91cb0cbe7006ff67c68
Shogiita <130059458+Shogiita@users.noreply.github.com>
2025-05-22 15:19:43 +0700

    login register logic

--- frontend/app/src/main/java/com/example/mindfulu/LoginFragment.kt
+++ frontend/app/src/main/java/com/example/mindfulu/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.mindfulu.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -14,8 +16,29 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSignIn.setOnClickListener(){
+            var username = binding.etUsernameLogin.text.toString()
+            var password = binding.etPasswordLogin.text.toString()
+
+            if(username.isEmpty() || password.isEmpty()){
+                Toast.makeText(context,"There's an Empty Field",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(context,"Login Success",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.buttonToSIgnUp.setOnClickListener(){
+            findNavController().navigate(R.id.action_global_registerFragment)
+        }
+    }
+
+
 
 }
