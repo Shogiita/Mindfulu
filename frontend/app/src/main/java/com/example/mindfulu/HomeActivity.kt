651399f7d0414f9f47a8d46d3141cd1c106a7dff
Edwardsus13 <88606166+Edwardsus13@users.noreply.github.com>
2025-06-21 00:13:50 +0700

    Create Home Activity
    With Include fragment, menu, navigation

---
+++ frontend/app/src/main/java/com/example/mindfulu/HomeActivity.kt
@@ -0,0 +1,42 @@
+package com.example.mindfulu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.mindfulu.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerHome) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.navigationHome.setOnItemSelectedListener {
+            if (it.itemId == R.id.mi_home_home){
+                navController.navigate(R.id.action_global_homeFragment)
+            } else if (it.itemId == R.id.mi_overview_home){
+                navController.navigate(R.id.action_global_overviewFragment)
+            } else if (it.itemId == R.id.mi_history_home){
+                navController.navigate(R.id.action_global_historyFragment)
+            } else if (it.itemId == R.id.mi_setting_home){
+                navController.navigate(R.id.action_global_settingFragment)
+            }
+            true
+        }
+    }
+}
